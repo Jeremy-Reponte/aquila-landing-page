eb09092663730b4878c04a5fb1cb20351ad7950f
Jason Te <te.jason.d@gmail.com>
2022-06-24 06:03:33 +0800

    :tada: Initial commit

---
+++ pages/about.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next/types";
+import React from "react";
+
+const about: NextPage = () => {
+  return (
+    <div>
+      <h1>This is about page</h1>
+    </div>
+  );
+};
+
+export default about;
